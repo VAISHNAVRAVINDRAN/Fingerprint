1f0f10339137d7858c732066a22589c9f610b8fa
VAISHNAVRAVINDRAN <104183316+VAISHNAVRAVINDRAN@users.noreply.github.com>
2022-04-28 08:05:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var fin1=document.querySelector(".finger1");
+var fin2=document.querySelector(".finger2");
+fin1.addEventListener("click", anime);
+function anime() {
+fin2.classList.add("anim");
+}
